39a98ce937a204179a0eaa5dc59c92f7c864ad26
Junskie07 <pbolabon@gmail.com>
2018-01-09 16:29:13 +0800

    BPO Reservation System GUI

---
+++ Reservation System/Reservation System/Reservation System/overall_sched-form.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class overall_sched_form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.close_label = New System.Windows.Forms.Label()
+        Me.b_about = New System.Windows.Forms.Button()
+        Me.b_edit_reservation = New System.Windows.Forms.Button()
+        Me.b_ovaerall_sched = New System.Windows.Forms.Button()
+        Me.b_reservation = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.b_home = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(6, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(231, 28)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Rooms Reservation"
+        '
+        'close_label
+        '
+        Me.close_label.AutoSize = True
+        Me.close_label.BackColor = System.Drawing.Color.Transparent
+        Me.close_label.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.close_label.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.close_label.ForeColor = System.Drawing.Color.White
+        Me.close_label.Location = New System.Drawing.Point(859, 5)
+        Me.close_label.Name = "close_label"
+        Me.close_label.Size = New System.Drawing.Size(30, 32)
+        Me.close_label.TabIndex = 9
+        Me.close_label.Text = "x"
+        '
+        'b_about
+        '
+        Me.b_about.BackColor = System.Drawing.Color.Transparent
+        Me.b_about.BackgroundImage = Global.Reservation_System.My.Resources.Resources.Button_01_green
+        Me.b_about.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.b_about.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.b_about.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.b_about.ForeColor = System.Drawing.Color.Transparent
+        Me.b_about.Location = New System.Drawing.Point(11, 481)
+        Me.b_about.Name = "b_about"
+        Me.b_about.Size = New System.Drawing.Size(163, 53)
+        Me.b_about.TabIndex = 14
+        Me.b_about.Text = "About" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.b_about.UseVisualStyleBackColor = False
+        '
+        'b_edit_reservation
+        '
+        Me.b_edit_reservation.BackColor = System.Drawing.Color.Transparent
+        Me.b_edit_reservation.BackgroundImage = Global.Reservation_System.My.Resources.Resources.Button_01_green
+        Me.b_edit_reservation.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.b_edit_reservation.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.b_edit_reservation.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.b_edit_reservation.ForeColor = System.Drawing.Color.Transparent
+        Me.b_edit_reservation.Location = New System.Drawing.Point(12, 203)
+        Me.b_edit_reservation.Name = "b_edit_reservation"
+        Me.b_edit_reservation.Size = New System.Drawing.Size(163, 53)
+        Me.b_edit_reservation.TabIndex = 13
+        Me.b_edit_reservation.Text = "Edit Reservation"
+        Me.b_edit_reservation.UseVisualStyleBackColor = False
+        '
+        'b_ovaerall_sched
+        '
+        Me.b_ovaerall_sched.BackColor = System.Drawing.Color.Transparent
+        Me.b_ovaerall_sched.BackgroundImage = Global.Reservation_System.My.Resources.Resources.Button_01_green
+        Me.b_ovaerall_sched.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.b_ovaerall_sched.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.b_ovaerall_sched.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.b_ovaerall_sched.ForeColor = System.Drawing.Color.Transparent
+        Me.b_ovaerall_sched.Location = New System.Drawing.Point(12, 144)
+        Me.b_ovaerall_sched.Name = "b_ovaerall_sched"
+        Me.b_ovaerall_sched.Size = New System.Drawing.Size(163, 53)
+        Me.b_ovaerall_sched.TabIndex = 12
+        Me.b_ovaerall_sched.Text = "Overall Sched" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.b_ovaerall_sched.UseVisualStyleBackColor = False
+        '
+        'b_reservation
+        '
+        Me.b_reservation.BackColor = System.Drawing.Color.Transparent
+        Me.b_reservation.BackgroundImage = Global.Reservation_System.My.Resources.Resources.Button_01_green
+        Me.b_reservation.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.b_reservation.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.b_reservation.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.b_reservation.ForeColor = System.Drawing.Color.Transparent
+        Me.b_reservation.Location = New System.Drawing.Point(12, 85)
+        Me.b_reservation.Margin = New System.Windows.Forms.Padding(0)
+        Me.b_reservation.Name = "b_reservation"
+        Me.b_reservation.Size = New System.Drawing.Size(163, 53)
+        Me.b_reservation.TabIndex = 11
+        Me.b_reservation.Text = "Reservation"
+        Me.b_reservation.UseVisualStyleBackColor = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.Controls.Add(Me.Chart1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.DarkGreen
+        Me.GroupBox1.Location = New System.Drawing.Point(196, 52)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(688, 495)
+        Me.GroupBox1.TabIndex = 15
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Overall Schedule"
+        '
+        'Chart1
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend1)
+        Me.Chart1.Location = New System.Drawing.Point(21, 33)
+        Me.Chart1.Name = "Chart1"
+        Me.Chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones
+        Series1.ChartArea = "ChartArea1"
+        Series1.Legend = "Legend1"
+        Series1.Name = "Series1"
+        Me.Chart1.Series.Add(Series1)
+        Me.Chart1.Size = New System.Drawing.Size(644, 435)
+        Me.Chart1.TabIndex = 0
+        Me.Chart1.Text = "Chart1"
+        '
+        'b_home
+        '
+        Me.b_home.BackColor = System.Drawing.Color.Transparent
+        Me.b_home.BackgroundImage = Global.Reservation_System.My.Resources.Resources.Button_01_green
+        Me.b_home.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.b_home.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.b_home.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.b_home.ForeColor = System.Drawing.Color.Transparent
+        Me.b_home.Location = New System.Drawing.Point(12, 422)
+        Me.b_home.Name = "b_home"
+        Me.b_home.Size = New System.Drawing.Size(163, 53)
+        Me.b_home.TabIndex = 22
+        Me.b_home.Text = "Home"
+        Me.b_home.UseVisualStyleBackColor = False
+        '
+        'overall_sched_form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Reservation_System.My.Resources.Resources.bg_main_admin1
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(896, 553)
+        Me.Controls.Add(Me.b_home)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.b_about)
+        Me.Controls.Add(Me.b_edit_reservation)
+        Me.Controls.Add(Me.b_ovaerall_sched)
+        Me.Controls.Add(Me.b_reservation)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.close_label)
+        Me.DoubleBuffered = True
+        Me.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.DarkGreen
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximumSize = New System.Drawing.Size(896, 553)
+        Me.MinimumSize = New System.Drawing.Size(896, 553)
+        Me.Name = "overall_sched_form"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "overall_sched_form"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents close_label As System.Windows.Forms.Label
+    Friend WithEvents b_about As System.Windows.Forms.Button
+    Friend WithEvents b_edit_reservation As System.Windows.Forms.Button
+    Friend WithEvents b_ovaerall_sched As System.Windows.Forms.Button
+    Friend WithEvents b_reservation As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Chart1 As System.Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents b_home As System.Windows.Forms.Button
+End Class
